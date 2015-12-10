6d08dff49b9e9ddd1f9b71e0bef00cd060aa4a1c
b-ritter <ben@benjaminritter.com>
2015-12-10 07:28:28 -0800

    Crude attempt to improve performance: optimize images and remove links to css

---
+++ gulpfile.js
@@ -0,0 +1,113 @@
+var gulp = require('gulp');
+
+// Testing
+var browserSync = require('browser-sync').create();
+var ngrok = require('ngrok');
+var psi = require('psi');
+var sequence = require('run-sequence');
+var site = '';
+var portVal = 3020;
+
+// Compiling js and sass
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
+var app_js_path = 'js/app_src/';
+var app_js = [
+
+];
+
+// Normal browser sync
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        port: portVal,
+        open: false,
+        server: {
+          baseDir: ''
+        }
+    });
+});
+
+// Set up testing
+gulp.task('browser-sync-psi', function() {
+    browserSync.init({
+        port: portVal,
+        open: false,
+        server: {
+          baseDir: ''
+        }
+    });
+});
+
+gulp.task('ngrok-url', function(cb) {
+  return ngrok.connect(portVal, function (err, url) {
+    site = url;
+    cb();
+  });
+});
+
+gulp.task('psi-desktop', function (cb) {
+  psi(site, {
+    nokey: 'true',
+    strategy: 'desktop'
+  }, cb);
+});
+
+gulp.task('psi-mobile', function (cb) {
+  psi(site, {
+    nokey: 'true',
+    strategy: 'mobile'
+  }, cb);
+});
+
+// psi sequence with 'browser-sync-psi' instead
+gulp.task('psi-seq', function (cb) {
+  return sequence(
+    'browser-sync-psi',
+    'ngrok-url',
+    'psi-desktop',
+    'psi-mobile',
+    cb
+  );
+});
+
+// psi task runs and exits
+gulp.task('psi', ['psi-seq'], function() {
+  process.exit();
+});
+
+app_js.forEach(function(currentValue, index, app_js){
+  app_js[index] = app_js_path + currentValue;
+});
+
+gulp.task('styles', function() {
+    gulp.src('scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+				.pipe(autoprefixer())
+        .pipe(gulp.dest('css/'));
+});
+
+gulp.task('concat-js', function(){
+  return gulp.src(app_js)
+    .pipe(concat('*.js'))
+    .pipe(rename('app.js'))
+    .pipe(gulp.dest('js/'));
+});
+
+gulp.task('watch', function(){
+ gulp.watch('scss/**/*.scss', ['styles']);
+ gulp.watch( app_js, ['concat-js']);
+});
+
+gulp.task('min', function(){
+	return gulp.src('js/**/*.js')
+		.pipe(uglify())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest(''));
+});
+
+
+
+gulp.task('default', [ 'watch' ]);
